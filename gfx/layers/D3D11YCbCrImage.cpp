6bfef39da047c5ca465a2d901f8e5e209413c124
trav90 <travawine@palemoon.org>
2023-11-29 07:56:15 -0600

    Issue #2393 - Part 7 - Add missing #include to D3D11YCbCrImage.cpp

--- gfx/layers/D3D11YCbCrImage.cpp
+++ gfx/layers/D3D11YCbCrImage.cpp
@@ -4,6 +4,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "D3D11YCbCrImage.h"
+
+#include "gfx2DGlue.h"
 #include "YCbCrUtils.h"
 #include "mozilla/gfx/gfxVars.h"
 #include "mozilla/layers/CompositableClient.h"
