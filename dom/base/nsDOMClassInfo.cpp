35311d664a83830004d31eebb64af501dbef4b36
Moonchild <moonchild@palemoon.org>
2024-04-03 13:59:06 +0200

    Issue #2490 - Part 12: Convert CSSStyleRule to WebIDL.
    The .style PutForwards bit is coming along for the ride here, aligning us with
    the behavior of Blink and Gecko.

--- dom/base/nsDOMClassInfo.cpp
+++ dom/base/nsDOMClassInfo.cpp
@@ -80,7 +80,6 @@
 #include "nsIDOMCSSSupportsRule.h"
 #include "nsIDOMCSSCounterStyleRule.h"
 #include "nsIDOMCSSPageRule.h"
-#include "nsIDOMCSSStyleRule.h"
 #include "nsIDOMXULCommandDispatcher.h"
 #include "nsIControllers.h"
 #include "nsITreeSelection.h"
@@ -186,9 +185,6 @@ static nsDOMClassInfoData sClassInfoData[] = {
   // Misc Core related classes
 
   // CSS classes
-  NS_DEFINE_CLASSINFO_DATA(CSSStyleRule, nsCSSRuleSH,
-                           DOM_DEFAULT_SCRIPTABLE_FLAGS |
-                           nsIXPCScriptable::WANT_PRECREATE)
   NS_DEFINE_CLASSINFO_DATA(CSSMediaRule, nsCSSRuleSH,
                            DOM_DEFAULT_SCRIPTABLE_FLAGS |
                            nsIXPCScriptable::WANT_PRECREATE)
@@ -506,11 +502,6 @@ nsDOMClassInfo::Init()
 
   DOM_CLASSINFO_MAP_BEGIN(DOMConstructor, nsIDOMDOMConstructor)
     DOM_CLASSINFO_MAP_ENTRY(nsIDOMDOMConstructor)
-  DOM_CLASSINFO_MAP_END
-
-  DOM_CLASSINFO_MAP_BEGIN(CSSStyleRule, nsIDOMCSSStyleRule)
-    DOM_CLASSINFO_MAP_ENTRY(nsIDOMCSSRule)
-    DOM_CLASSINFO_MAP_ENTRY(nsIDOMCSSStyleRule)
   DOM_CLASSINFO_MAP_END
 
   DOM_CLASSINFO_MAP_BEGIN(CSSMediaRule, nsIDOMCSSMediaRule)
@@ -1986,14 +1977,6 @@ nsCSSRuleSH::PreCreate(nsISupports *nativeObj, JSContext *cx,
     return NS_ERROR_UNEXPECTED;
   }
   css::Rule* cssRule = rule->GetCSSRule();
-  if (!cssRule) {
-    // A DOMCSSStyleRule whose actual underlying rule has gone away.  There
-    // isn't much a caller can do with this thing anyway, and only chrome code
-    // can get its hands on it to start with, so just wrap in the current
-    // global.
-    *parentObj = globalObj;
-    return NS_OK;
-  }
   nsIDocument* doc = cssRule->GetDocument();
   if (!doc) {
     *parentObj = globalObj;
