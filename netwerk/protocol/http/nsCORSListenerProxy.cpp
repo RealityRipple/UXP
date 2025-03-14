a343bf8cd33795a48693d31d8232dcaf1127ff56
Shadow <shadow@no-reply.palemoon.org>
2025-03-14 14:40:41 +0000

    Issue #2703 - Part 4: Fix incorrect clang warning in nsCORSListenerProxy.
    See BZ Bug 1444472
    Resolves #2703

--- netwerk/protocol/http/nsCORSListenerProxy.cpp
+++ netwerk/protocol/http/nsCORSListenerProxy.cpp
@@ -249,7 +249,7 @@ nsPreflightCache::CacheEntry::CheckRequest(const nsCString& aMethod,
     }
   }
 
-  const struct CheckHeaderToken {
+  struct CheckHeaderToken {
     bool Equals(const TokenTime& e, const nsCString& header) const {
       return e.token.Equals(header, comparator);
     }
