1f15176b70ace06a2888880fdfecb5afdd581182
Shadow <shadow@no-reply.palemoon.org>
2025-05-08 10:03:06 +0000

    No Issue - Remove obsolete pref.
    Bug 1193695
    
    Removes obsolete preference "gfx.prefer-mesa-llvmpipe"
    
    The code which made use of it was removed.

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -786,7 +786,6 @@ pref("gfx.canvas.skiagl.dynamic-cache", true);
 pref("gfx.text.disable-aa", false);
 
 pref("gfx.work-around-driver-bugs", true);
-pref("gfx.prefer-mesa-llvmpipe", false);
 
 pref("gfx.draw-color-bars", false);
 
